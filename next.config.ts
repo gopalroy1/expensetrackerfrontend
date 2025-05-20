eecfd9dd563f6f68b7d7ad5163c3f5f665f40d01
gopalaegis <gopal@aegiscovenant.com>
2025-05-20 08:32:34 +0530

    next config set up

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
+import { IS_ENVIRONMENT_DEV } from "@/app/env";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators:false
+  devIndicators:IS_ENVIRONMENT_DEV===true?false:IS_ENVIRONMENT_DEV
 };
 
 export default nextConfig;
---
+++ src/app/env.ts
@@ -0,0 +1,3 @@
+import { DEVELOPMENT_ENV } from "./constant.js";
+
+export const IS_ENVIRONMENT_DEV = process.env.ENV===DEVELOPMENT_ENV || undefined
